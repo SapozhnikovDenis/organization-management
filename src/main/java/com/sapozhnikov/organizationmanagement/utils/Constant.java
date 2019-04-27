4280d45bb0cf7e9571c2d40ea56db120f9c2542a
sapojnikovdn <sapojnikovdn@sovcombank.ru>
2019-04-27 20:21:39 +0400

    add rest service for rename department

--- src/main/java/com/sapozhnikov/organizationmanagement/utils/Constant.java
+++ src/main/java/com/sapozhnikov/organizationmanagement/utils/Constant.java
@@ -1,5 +1,7 @@
 package com.sapozhnikov.organizationmanagement.utils;
 
-public class Constants {
+public class Constant {
+    private Constant() {}
     public static final String API_V_1_DEPARTMENT_URL = "/api/v1/department";
+    public static final String RENAME_URL = "/rename";
 }
--- src/main/java/com/sapozhnikov/organizationmanagement/web/controller/rest/DepartmentController.java
+++ src/main/java/com/sapozhnikov/organizationmanagement/web/controller/rest/DepartmentController.java
@@ -1,6 +1,7 @@
 package com.sapozhnikov.organizationmanagement.web.controller.rest;
 
 import com.sapozhnikov.organizationmanagement.web.dto.department.CreateDepartmentRq;
+import com.sapozhnikov.organizationmanagement.web.dto.department.RenameDepartmentRq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-import static com.sapozhnikov.organizationmanagement.utils.Constants.API_V_1_DEPARTMENT_URL;
+import static com.sapozhnikov.organizationmanagement.utils.Constant.API_V_1_DEPARTMENT_URL;
+import static com.sapozhnikov.organizationmanagement.utils.Constant.RENAME_URL;
 
 @Slf4j
 @RestController
@@ -21,6 +23,12 @@ public class DepartmentController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<Void> createDepartment(@RequestBody @Valid CreateDepartmentRq createDepartmentRq) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(value = RENAME_URL, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Void> renameDepartment(@RequestBody @Valid RenameDepartmentRq renameDepartmentRq) {
         return ResponseEntity.ok().build();
     }
 }
